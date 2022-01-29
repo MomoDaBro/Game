7144b40e493b86eb49bdc45aea039e49513e4c6b
MomoDaBro <Momofqais@gmail.com>
2022-01-28 22:43:56 -0600

    Added an MD to my file

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-// There will be 3 main parts to each level
-// the first part will be the character you play as
-// The componets that make up the character stage are as follows
-// Hitpoints, how much health you currently have should be stated after every turn
-// Turn Options, the four actions that can be chosen during your turn
-// These four options are Attack, dealing a static amount of damage unless increased with means later on mentioned (maybe crits..?)
-// Blocking is the second option, allowing the user to take less damage, this amount can also be scaled later through means
-// Item, allowing a user to use an item in their inventory, this action does not consume a turn
-// Escape, Escape essentially ends the game allowing a user to restart.
 
-// Part two consists of the enemies that you will face upon playing.
-// The compentnets that make up the enemies are as follows
-// First the number of enemies that you take on at once will be determined
-// Easier and beginner levels will contain one enemy while as you progress you can get more enemies to appear
-// Damage: Enemies will deal static damage unless affected by an item, harder enemies deal more damage
-// There will be three base types of enemies, a melee unit, a ranged unit, and a magic unit.
-// Bosses may be implemted but not fully thought out just yet.
-
-// The last part is items as previously mentioned.
-// After an enemy is slain it will have a chance of dropping an item based on its class.
-// The current items accessible are, Regen potions, Defense up, Attack up, Clear Effect, Spell card
 
     public static void main(String[] args) {
 

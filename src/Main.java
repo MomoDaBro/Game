2c576ab3d28d7a5ea3d8d61260ed160a02d7d3e3
MomoDaBro <Momofqais@gmail.com>
2022-01-28 23:43:06 -0600

    Added new enemy units, as well as a parry mechanic for blocking to make it favorable compared to attacking

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-
 
 
     public static void main(String[] args) {
@@ -9,8 +9,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int HRbaseHealthPoints = 100;
         int HRbaseDamage = 10;
-        int ENbaseHP = 30;
-        int ENbaseDamage = 10;
+        int EN0baseHP = 30;
+        int EN0baseDamage = 10;
+        String EN0name = "Melee unit";
+        int EN1baseHP = 15;
+        int EN1baseDamage = 30;
+        String EN1name = "Ranged unit";
+        int EN2baseHP = 30;
+        int EN2baseDamage = 20;
+        String EN2name = "Magic unit";
         int shieldDamageReduction = 5;
 
 
@@ -18,23 +25,50 @@ public class Main {
         String characterName = scanner.nextLine();
         System.out.println(characterName + " enters the monster dungeon, will they survive?");
         System.out.println("尺ㄖㄖ爪 1");
-        System.out.println("A melee unit appears!");
+        int ENtype = ThreadLocalRandom.current().nextInt(3);
 
-        int ENspecificHP = ENbaseHP;
+        int ENspecificHP;
+        int ENspecificATK;
+        String ENspecificName;
+        if (ENtype == 0) {
+            ENspecificHP = EN0baseHP;
+            ENspecificATK = EN0baseDamage;
+            ENspecificName = EN0name;
+        } else {
+            if (ENtype == 1) {
+                ENspecificHP = EN1baseHP;
+                ENspecificATK = EN1baseDamage;
+                ENspecificName = EN1name;
+            } else {
+                ENspecificHP = EN2baseHP;
+                ENspecificATK = EN2baseDamage;
+                ENspecificName = EN2name;
+            }
+
+        }
+
         int HRspecificHP = HRbaseHealthPoints;
+        int HRcounterDamage = HRbaseDamage - 5;
         while (true) {
-            int roundENspecificATK = ENbaseDamage;
-            System.out.println("Melee Unit currently has " + ENspecificHP + " health");
+            int roundENspecificATK = ENspecificATK;
+            System.out.println(ENspecificName + " currently has " + ENspecificHP + " health");
             System.out.println(characterName + " currently has " + HRspecificHP + " health");
             System.out.println("Choose 1 to attack, 2 for block, 3 for items, 4 for escape");
             String characterInput = scanner.nextLine();
             int action = Integer.parseInt(characterInput);
             if (action == 1) {
-                System.out.println(characterName + " attacks melee unit for " + HRbaseDamage + " damage!");
+                System.out.println(characterName + " attacks " + ENspecificName + " for " + HRbaseDamage + " damage!");
                 ENspecificHP -= HRbaseDamage;
             } else if (action == 2) {
                 System.out.println(characterName + " blocks the attack!");
-                roundENspecificATK -= 3;
+                int HRblockChance = ThreadLocalRandom.current().nextInt(2);
+                if (HRblockChance == 0) {
+                    roundENspecificATK = 0;
+                    System.out.println(characterName + " counters " + ENspecificName + " for " + HRbaseDamage + " damage!");
+                } else {
+                    roundENspecificATK -= 3;
+                }
+
             } else if (action == 4) {
                 System.out.println(characterName + " decided to run away!");
                 System.out.println(characterName + " is a coward!");
@@ -43,11 +77,11 @@ public class Main {
             }
 
             if (ENspecificHP <= 0) {
-                System.out.println("Melee Unit has been slain!");
+                System.out.println(ENspecificName + " has been slain!");
                 break;
             }
             HRspecificHP -= roundENspecificATK;
-            System.out.println("The melee unit attacks!");
+            System.out.println("The " + ENspecificName + " attacks!");
             System.out.println(characterName + " has taken " + roundENspecificATK + " damage!");
 
         }

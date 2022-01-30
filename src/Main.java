b5ed629a36172744141cc482d0a36638d85398c7
momoqais <momofqais@gmail.com>
2022-01-29 19:50:21 -0600

    Fixed the block damage reduction, also added a temporary form of a 2nd room which functions

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int EN2baseHP = 25;
         int EN2baseDamage = 25;
         String EN2name = "Magic unit";
-        int shieldDamageReduction = 5;
+
 
 
         System.out.println("Please name your character");
@@ -46,11 +46,84 @@ public class Main {
             }
 
         }
-
+        System.out.println("A " + ENspecificName + " appears!");
         int HRspecificHP = HRbaseHealthPoints;
         int HRcounterDamage = HRbaseDamage - 5;
         float FloatENspecificATK;
         int convertENspecificATK;
+        while (true) {
+            int roundENspecificATK = ENspecificATK;
+            System.out.println(ENspecificName + " currently has " + ENspecificHP + " health");
+            System.out.println(characterName + " currently has " + HRspecificHP + " health");
+            System.out.println("Choose 1 to attack, 2 for block, 3 for items, 4 for escape");
+            String characterInput = scanner.nextLine();
+            int action = Integer.parseInt(characterInput);
+            if (action == 1) {
+                System.out.println(characterName + " attacks " + ENspecificName + " for " + HRbaseDamage + " damage!");
+                ENspecificHP -= HRbaseDamage;
+                if (ENspecificHP >= 0){
+                    System.out.println("The " + ENspecificName + " attacks!");
+                }
+            } else if (action == 2) {
+                System.out.println(characterName + " blocks the attack!");
+                int HRblockChance = ThreadLocalRandom.current().nextInt(3);
+                if (HRblockChance == 0) {
+                    roundENspecificATK = 0;
+                    System.out.println(characterName + " counters " + ENspecificName + " for " + HRbaseDamage + " damage!");
+                    ENspecificHP -= HRbaseDamage;
+                } else {
+                    FloatENspecificATK = ENspecificATK;
+                    convertENspecificATK = (int) (FloatENspecificATK * (40.0f/100.0f));
+                    roundENspecificATK = convertENspecificATK;
+                    System.out.println("The " + ENspecificName + " attacks!");
+                }
+
+            } else if (action == 4) {
+                System.out.println(characterName + " decided to run away!");
+                System.out.println(characterName + " is a coward!");
+                System.out.println("₲₳₥Ɇ ØVɆⱤ");
+                break;
+            }
+
+            if (ENspecificHP <= 0) {
+                System.out.println(ENspecificName + " has been slain!");
+                System.out.println("尺ㄖㄖ爪 2");
+                break;
+            }
+            HRspecificHP -= roundENspecificATK;
+
+            System.out.println(characterName + " has taken " + roundENspecificATK + " damage!");
+            if (HRspecificHP <= 0){
+                System.out.println(characterName + " has been slain!");
+                System.out.println("₲₳₥Ɇ ØVɆⱤ");
+                break;
+
+            }
+
+        }
+
+        //Should each room be its own seperate loop?
+        //If i do that, each room will need its own values for enemies health to make it harder
+        //Or i could take the base health and scale it up like how i did with block damage (but reverse)
+
+        ENtype = ThreadLocalRandom.current().nextInt(3);
+        if (ENtype == 0) {
+            ENspecificHP = EN0baseHP;
+            ENspecificATK = EN0baseDamage;
+            ENspecificName = EN0name;
+        } else {
+            if (ENtype == 1) {
+                ENspecificHP = EN1baseHP;
+                ENspecificATK = EN1baseDamage;
+                ENspecificName = EN1name;
+            } else {
+                ENspecificHP = EN2baseHP;
+                ENspecificATK = EN2baseDamage;
+                ENspecificName = EN2name;
+            }
+
+        }
+        System.out.println("A " + ENspecificName + " appears!");
         while (true) {
             int roundENspecificATK = ENspecificATK;
             System.out.println(ENspecificName + " currently has " + ENspecificHP + " health");
@@ -71,7 +144,7 @@ public class Main {
                     ENspecificHP -= HRbaseDamage;
                 } else {
                     FloatENspecificATK = ENspecificATK;
-                    convertENspecificATK = (int) (FloatENspecificATK * (2/5));
+                    convertENspecificATK = (int) (FloatENspecificATK * (40.0f/100.0f));
                     roundENspecificATK = convertENspecificATK;
                     System.out.println("The " + ENspecificName + " attacks!");
                 }
@@ -98,7 +171,6 @@ public class Main {
             }
 
         }
-
 
     }
 
